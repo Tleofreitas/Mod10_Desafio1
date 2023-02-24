375e5754ed03b43e3c0d9518cc2064977d99f56e
Thiago <thiagoleonardo1995@live.com>
2023-02-24 12:33:16 -0300

    Invitation Component

---
+++ src/components/Invitation/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function Invitation() {
+    return (
+        <div className='dsct-invitation'>
+            <h1>Venha nos visitar</h1>
+        </div>
+    );
+}
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,7 +1,20 @@
 import Header from "../../components/Header";
+import Invitation from "../../components/Invitation";
 
 export default function Catalog() {
-    return(
-        <Header />
+    return (
+        <>
+            <Header />
+            <main className="dsct-catalog-main"> 
+                <section id="dsct-catalog-section" className='dsct-container'>
+                    <div className="dsct-invitation-container">
+                        <Invitation />
+                    </div>
+                    <div className="dsct-catalog-cards-container">
+
+                    </div>
+                </section>
+            </main>
+        </>
     );
 }
